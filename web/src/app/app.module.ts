07dd064dd67d6880ff80087dd7e728641f03329c
ShrootBuck <zaydkrunz@gmail.com>
2023-05-11 20:46:51 -0700

    done!

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 // MUI Components
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatButtonModule } from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 // Pages
@@ -29,11 +31,13 @@ import { TopBarComponent } from './components/top-bar/top-bar.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
     MatToolbarModule,
     MatButtonModule,
     MatProgressSpinnerModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- web/src/app/pages/index/index.component.ts
+++ web/src/app/pages/index/index.component.ts
@@ -1,27 +1,64 @@
 import { Component } from '@angular/core';
-import { Configuration, OpenAIApi } from "openai";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-
-const configuration = new Configuration({
-  organization: "org-YFfyErm4NuAqFGTRJGV7BI5R",
-  apiKey: "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",
-});
-
-const openai = new OpenAIApi(configuration);
-
-interface OutputItem {
-  message: {},
-  image?: string,
+interface Message {
+  role: string;
+  content: string;
 }
 
-const starting_messages = [
-  {
-    role: "system",
-    content: "You are the author in a creative story-telling experience. Offer the user a set of numbered choices after each turn. Use imagery to depict the scenes."},
+interface OutputItem {
+  message: Message;
+  image?: string;
+}
+
+interface OpenAIChatResponse {
+  id: string;
+  object: string;
+  created: Number;
+  choices: [
     {
-      role: "assistant",
-      content: "You are the author in a creative story-telling experience. Offer the user a set of numbered choices after each turn. Use imagery to depict the scenes."},
-]
+      index: Number;
+      message: Message;
+      finish_reason: string;
+    }
+  ];
+  usage: {
+    prompt_tokens: Number;
+    completion_tokens: Number;
+    total_tokens: Number;
+  };
+}
+
+interface OpenAIImageResponse {
+  created: Number;
+  data: [
+    {
+      url: string;
+    }
+  ];
+}
+
+const http_options = {
+  headers: new HttpHeaders({
+    Authorization: 'Bearer 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',
+  }),
+};
+
+const story_starters: string[] = [
+  "You have been locked in a cold room in an unfamiliar house. Before we begin, what's your name?",
+  "You wake up on the ground in an abandoned warehouse. Before we begin, what's your name?",
+];
+
+const system_message = {
+  role: 'system',
+  content:
+    'You are the author in a creative story-telling experience. Offer the user suggestions after each turn. Use imagery to depict the scenes.',
+};
+
+const starting_message = {
+  role: 'assistant',
+  content: story_starters[Math.floor(Math.random() * story_starters.length)],
+};
 
 @Component({
   selector: 'app-index',
@@ -31,17 +68,63 @@ const starting_messages = [
 export class IndexComponent {
   loading: boolean = false;
 
-  output: OutputItem[] = [
-    {
-      message: starting_message,
-    }
-  ];
+  output: OutputItem[] = [{ message: starting_message }];
+  messages: Message[] = [system_message, starting_message];
 
-  messages: {} = {starting_message};
+  textbox: string = '';
 
   async onEnterKey() {
     this.loading = true;
 
-    this.loading = false;
+    this.messages.push({ role: 'user', content: this.textbox });
+
+    const message_context = {
+      model: 'gpt-3.5-turbo',
+      messages: this.messages,
+    };
+
+    this.http
+      .post<OpenAIChatResponse>(
+        'https://api.openai.com/v1/chat/completions',
+        message_context,
+        http_options
+      )
+      .subscribe((data) => {
+        this.messages.push(data.choices[0].message);
+
+        const visual_message_context = {
+          model: 'gpt-3.5-turbo',
+          messages: this.messages.concat({
+            role: 'user',
+            content: 'Reply with a visual description of the current scene.',
+          }),
+        };
+
+        this.http
+          .post<OpenAIChatResponse>(
+            'https://api.openai.com/v1/chat/completions',
+            visual_message_context,
+            http_options
+          )
+          .subscribe((data) => {
+            this.http
+              .post<OpenAIImageResponse>(
+                'https://api.openai.com/v1/images/generations',
+                { prompt: data.choices[0].message.content, size: '512x512' },
+                http_options
+              )
+              .subscribe((data) => {
+                this.output.push({
+                  message: this.messages[this.messages.length - 1],
+                  image: data.data[0].url,
+                });
+
+                this.loading = false;
+                this.textbox = '';
+              });
+          });
+      });
   }
+
+  constructor(private http: HttpClient) {}
 }
